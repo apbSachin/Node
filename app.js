e1fcd95b0e864f468d0b46a663658e7293e4c319
sachin <sba40599@gmail.com>
2023-09-25 22:17:03 -0700

    .

---
+++ app.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+
+const add=math.add(2,6);
+const subst=math.subtract(10,5);
+
+console.log(`sum is ${add}`);
+console.log(`substract is ${subst}`);
---
+++ math.js
@@ -0,0 +1,13 @@
+//node ll work on modules math is one module
+function add(a, b) {
+  return a + b;
+}
+
+function substract(x, y) {
+  return x - y;
+}
+
+module.exports={
+    add,
+    substract
+}
